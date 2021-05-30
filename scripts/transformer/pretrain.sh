330ae1b0fd4e1f5260a87a7a11b0d135cb77b9ec
kzCassie <kz751@nyu.edu>
2021-05-30 20:56:42 +0100

    transformer shell

--- scripts/transformer/pretrain.sh
+++ scripts/transformer/pretrain.sh
@@ -3,7 +3,6 @@ set -e
 #set -x
 
 mined_num=$1
-#parser="transformer_parser"
 parser="transformer_enc_parser"
 
 echo "Training using ${mined_num} mined data."
@@ -18,6 +17,8 @@ finetune_file="data/conala/${mined_num}/train.gold.full.bin"
 
 seed=0
 dropout=0.3
+enc_nhead=4
+enc_nlayer=2
 hidden_size=256
 embed_size=128
 action_embed_size=128
@@ -26,12 +27,14 @@ type_embed_size=64
 lr=0.001
 lr_decay=0.5
 batch_size=64
-max_epoch=80
+max_epoch=120
+patience=20
+max_num_trial=10
 beam_size=15
 lstm='lstm'  # lstm
 lr_decay_after_epoch=15
 valid_every_epoch=5
-model_name=${parser}.hidden${hidden_size}.embed${embed_size}.action${action_embed_size}.field${field_embed_size}.type${type_embed_size}.dr${dropout}.lr${lr}.lr_de${lr_decay}.lr_da${lr_decay_after_epoch}.beam${beam_size}.$(basename ${vocab}).$(basename ${train_file}).glorot.par_state.seed${seed}
+model_name=${parser}.enc_nhead${enc_nhead}.enc_nlayer${enc_nlayer}.hidden${hidden_size}.embed${embed_size}.action${action_embed_size}.field${field_embed_size}.type${type_embed_size}.dr${dropout}.lr${lr}.lr_de${lr_decay}.lr_da${lr_decay_after_epoch}.beam${beam_size}.$(basename ${vocab}).$(basename ${train_file}).glorot.par_state.seed${seed}
 
 echo "**** Writing results to logs/conala/${model_name}.log ****"
 mkdir -p logs/conala
@@ -39,7 +42,7 @@ echo commit hash: `git rev-parse HEAD` > logs/conala/${model_name}.log
 
 python -u exp.py \
     --parser ${parser} \
-    --cuda ${cuda} \
+    --cuda \
     --seed ${seed} \
     --mode train \
     --batch_size ${batch_size} \
@@ -58,8 +61,8 @@ python -u exp.py \
     --field_embed_size ${field_embed_size} \
     --type_embed_size ${type_embed_size} \
     --dropout ${dropout} \
-    --patience 5 \
-    --max_num_trial 5 \
+    --patience ${patience} \
+    --max_num_trial ${max_num_trial} \
     --glorot_init \
     --lr ${lr} \
     --lr_decay ${lr_decay} \
@@ -68,6 +71,8 @@ python -u exp.py \
     --beam_size ${beam_size} \
     --log_every 50 \
     --valid_every_epoch ${valid_every_epoch} \
+    --enc_nhead ${enc_nhead} \
+    --enc_nlayer ${enc_nlayer} \
     --save_to saved_models/conala/${model_name} 2>&1 | tee logs/conala/${model_name}.log
 
 . scripts/transformer/test.sh saved_models/conala/${model_name}.bin ${mined_num} ${parser} 2>&1 | tee -a logs/conala/${model_name}.log
